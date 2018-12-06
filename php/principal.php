21645e6e6f9c5d696e732421aafd9034b4154e41
Patricia56 <38794424+Patricia56@users.noreply.github.com>
2018-12-05 18:21:00 -0600

    Inserccion de imagen

--- php/principal.php
+++ php/principal.php
@@ -16,6 +16,16 @@ require_once("myDBC.php");
 </font>
 <br><font face="Arial" size="+1" color="#FFF6F2" ><a href="salir.php"style="float:right" >Cerra Sesion</a></font></br>
     <div><center><h2>Seleccione el departamento al que desea llegar</h2><center> </div>  
+<map name="mapeo">
+            <area alt="Si clicas aqui iras al mapa" shape="rect"  coords="48,317,369,389"  href="Funciones.php">
+            <area  shape="rect" coords="470,272,569,305"  href="Funciones.php">
+           <area  shape="rect" coords="230,270,329,303"  href="Funciones.php">
+          <area  shape="rect" coords="229,121,331,156"href="Funciones.php"> 
+          <area  shape="rect" coords="-2,121,92,154"  href="Funciones.php">
+          <area  shape="rect" coords="-4,267,91,302"  href="Funciones.php">
+          <area  shape="rect" coords="475,122,576,158"  href="Funciones.php">
+         </map>
+  <center><img src="HOLA.gif" usemap="#mapeo" ></center>
 </body>
 </html>
 <?php
