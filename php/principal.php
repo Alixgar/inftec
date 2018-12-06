5e66226b5007f4a19c5c9d33407e21227b06f187
Patricia56 <38794424+Patricia56@users.noreply.github.com>
2018-12-05 18:16:55 -0600

    funcion de letras

--- php/principal.php
+++ php/principal.php
@@ -1,24 +1,28 @@
 <?php
 require_once("myDBC.php");
-if(isset($_SESSION['session']))
-{
+  session_start();
+  if(isset($_SESSION['session'])){
+
 ?>
 <!DOCTYPE html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>..:PAGINA PRINCIPAL:..</title>
-</head>
 
-<body bgcolor="#e0e3e3">
-	<center><h2>BIENVENIDO</h2></center>
-	
-	<a href="salir.php"><h4>CERRAR SESSION</h4></a>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ </head>
+<body bgcolor="#009966">
+<font size="20" face ="Snap ITC" color="blue">
+<marquee behavior =alternate>Bienvenido</marquee>
+</font>
+<br><font face="Arial" size="+1" color="#FFF6F2" ><a href="salir.php"style="float:right" >Cerra Sesion</a></font></br>
+    <div><center><h2>Seleccione el departamento al que desea llegar</h2><center> </div>  
 </body>
 </html>
 <?php
 }else
+
 echo'<script type="text/javascript">
 	  alert("Registrarse para ver este contenido");
-	  window.location="http://localhost/login/index.php"
+	  window.location="../index.php"
 </script>';
 ?>
