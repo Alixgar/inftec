c91caa3da7985e6f361d53c3476754a4e561ca81
Patricia56 <38794424+Patricia56@users.noreply.github.com>
2018-12-05 23:11:21 -0600

    Archivo funciones

--- php/Funciones.php
+++ php/Funciones.php
@@ -4,7 +4,9 @@
  </head>
  <body>
 <div> <font face="Arial" size="+1" color="#FF0000" ><a href="principal.php"style="float:right" >Regresar</a></font></div>
-
+  <font size="20" face ="Snap ITC" color="blue">
+<marquee behavior =alternate>Gracias por seleccionar nuestro sistema</marquee>
+<center><img src="../imagenes/Plano Tec 2018.png"  ></center>
  </body>
  
 </html>
