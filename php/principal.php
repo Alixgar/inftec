3a32196b02886eb70192839a3db0020eed62e7d0
Patricia56 <38794424+Patricia56@users.noreply.github.com>
2018-12-05 23:04:26 -0600

    cambio de letras

--- php/principal.php
+++ php/principal.php
@@ -7,11 +7,15 @@ if(isset($_SESSION['session']))
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>..:PAGINA PRINCIPAL:..</title>
-</head>
 
-<body bgcolor="#e0e3e3">
-	<center><h2>BIENVENIDO</h2></center>
-	
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ </head>
+<body bgcolor="#009966">
+<font size="20" face ="Snap ITC" color="blue">
+<marquee behavior =alternate>Bienvenido</marquee>
+</font>
+<br><font face="Arial" size="+1" color="#FFF6F2" ><a href="salir.php"style="float:right" >Cerra Sesion</a></font></br>
+    <div><center><h2>Seleccione el departamento al que desea llegar</h2><center> </div>  
 	<a href="salir.php"><h4>CERRAR SESSION</h4></a>
 </body>
 </html>
