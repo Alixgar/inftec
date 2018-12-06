71add075cfc3d8c360c49175966341389a7ee471
Patricia56 <38794424+Patricia56@users.noreply.github.com>
2018-12-05 23:09:18 -0600

    Nuevo archivo Funciones

---
+++ php/Funciones.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>Pagina de mapas </title>
+ </head>
+ <body>
+<div> <font face="Arial" size="+1" color="#FF0000" ><a href="principal.php"style="float:right" >Regresar</a></font></div>
+
+ </body>
+ 
+</html>
