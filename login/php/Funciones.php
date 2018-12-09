cd961dd49852dc675dd28879b17c967433893bee
Alicia <alysszgarcia@gmail.com>
2018-12-09 00:08:29 -0600

    Funcion del segundo modulo

--- login/php/Funciones.php
+++ login/php/Funciones.php
@@ -3,8 +3,19 @@
  <meta charset="UTF-8">
 	<body bgcolor="#708090">
   <title>Pagina de mapas </title>
- </head> <body>
- 
+ </head>
+ <body>
+ <br><font face="Arial" size="+1" color="#FFFFFF" ><a href="salir.php"style="float:right" >Cerra Sesion</a></font></br>
+<br> <font face="Arial" size="+1" color="#FF0000" ><a href="principal.php"style="float:right" >Regresar</a></font></br>
+  <font size="20" face ="Snap ITC" color="blue">
+  <center>	
+			<header>
+		<div class="header-top">
+			<div class="navegacion">
+				<input type="search" placeholder="Buscar . . ." id="inputBusqueda">
+  </div></div></center>
+<marquee behavior =alternate>Indique su ruta</marquee>
+<center><img src="../imagenes/Plano Tec 2018.png"  ></center>
 </header>
  </body>
  
