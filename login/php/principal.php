543e7fab6cb32bcd6bb585e755143157637ee643
Alicia <alysszgarcia@gmail.com>
2018-12-09 00:04:15 -0600

    Buscadordor

--- login/php/principal.php
+++ login/php/principal.php
@@ -1,48 +1,109 @@
 <?php
 require_once("myDBC.php");
-if(isset($_SESSION['session']))
-{
+  session_start();
+  if(isset($_SESSION['session'])){
 ?>
 <!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>..:PAGINA PRINCIPAL:..</title>
-</head>
-
-<body bgcolor="#F5A9F2">
-	<center><h2>BIENVENIDO</h2></center>
-	<center> <div>
-	<a href="Funciones.php"><img src="1.png"></a>
+	<meta charset="UTF-8">
+	<body bgcolor="#FFC0CB">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
+	<title>Buscador</title>
+	<link rel="stylesheet" type="text/css" href="../css/style2.css">
+	</head>
+	<body>
+	<?php
+		echo $_SESSION['id']
+		?>
+		<center>
+				<br><font face="Arial" size="+1" ><a href="salir.php"style="float:right" >Cerra Sesion</a></font></br>					
+		<div class="wrapper">
+			<div id="title">
+			<center><h1>seleccione una imagen para conocer los departamentos </h1></center>
+				<input id="search-box" type="text" name="search">
+			</div>
+			<div id="directory-cont"><ul id="directory">
+						 <center> <div>              
+				 <table>
+					<tr>
+						<td>
+							<div></div>
+						<div class="wrap">
+						<div></div>
+ 
+				 <center> <div>              
+				 
+                   <img src="ISC.jpg"></a>
 					  <p class="name"><a href="Funciones.php">sistemas computacionales</a></p>
 						<p>&#9742 1111 - 1111</p>
 				  </div></center>
-				  <center> <img src="2.png">
-                       <p class="name">
-                         <a href="Funciones.php">ingenieria civil</a></p>
+				  <td></td>
+				  <td></td>
+				</td>
+				<td>        
+				<center><div>  
+					  <img src="IC.jpg">
+                       <p class="name"><a href="Funciones.php">ingenieria civil</a></p>
                        <p>&#9742 2222 - 2222</p>
                        </div>
 					</center>
+					</td>
+</tr>
+<tr>
+<td>
+                 <div>
 					<center>
-					<img src="3.png">
-					<p class="name">
-                <a href="Funciones.php">ingenieria gestion</a></p>
+				<img src="IGE.jpg">
+					<p class="name"><a href="Funciones.php">ingenieria en gestion</a></p>
 							<p>&#9742 3333 - 3333</p>
 						</div> 
 						</center>
-						<center>
-					<img src="4.png">						
-                	<p class="name"> 
-                <a href="Funciones.php">ingenieria gestion</a></p>
+						<td></td>
+				  <td></td>			  					
+							
+</td>
+<td>
+	
+					          <div>
+					<center>
+					<img src="II.jpg">						
+                	<p class="name"><a href="Funciones.php">ingenieria industrial</a></p>
 							<p>&#9742 4444 - 4444</p>
 						</div>
 					</center>
-	<a href="salir.php"><h4>CERRAR SESSION</h4></a>
-</body>
+					</td>
+</tr>
+<tr>
+<td>
+                 <div>
+					<center>
+					<img src="IM.jpg">
+					<p class="name"><a href="Funciones.php">ingenieria mecatronica</a></p>
+							<p>&#9742 3333 - 3333</p>
+						</div> 
+						</center>
+						<td></td>
+				  <td></td>			  					
+							
+</td>
+<td>
+	
+					          <div>
+					<center>
+
+					<img src="admin.png">						
+                	<p class="name"><a href="Funciones.php">Administracion</a></p>
+							<p>&#9742 4444 - 4444</p>
+						</div>
+					</center>
+				</ul>
+  </header>
+  </div>				<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+		<script src="../js/jquery.liveFilter.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			$('#directory').liveFilter('#search-box', 'td', { filterChildSelector: '.name'});
+		</script>
+	</body>	
 </html>
 <?php
-}else
-echo'<script type="text/javascript">
-	  alert("Registrarse para ver este contenido");
-	  window.location="http://localhost/infetec/login/index.php"
-</script>';
-?>
