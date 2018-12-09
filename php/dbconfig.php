64078fd8f02b4b56be0692796f82fa7632f7e1a3
frezzenboy@gmail.com <39112635+deuslux12@users.noreply.github.com>
2018-12-08 18:15:32 -0600

    conexion

--- php/dbconfig.php
+++ php/dbconfig.php
@@ -1,6 +1,23 @@
 <?php
-	define("DB_SERVER", "localhost");
-	define("DB_USER", "root");
-	define("DB_PASS", "admin");
-	define("DB_NAME", "login");
+ class conection{
+   // Conexion a la base de datos mediante PDO
+     public static function get_conection(){
+        //variables para la conexion
+        $user="paul"; $pass=".4V-2D7+=7:QMF"; $host="localhost"; $dbname="ittlaxia_infotec";
+        //Se intenta conectar con los datos asignados
+        try{
+            //se crea la instancia PDO con sus parametros de conexion
+            $conexion= new PDO("mysql:host=$host;dbname=$dbname;",
+            $user,$pass);
+            //Se retorna la conexion una vez realizada
+            return $conexion;
+        }
+        //Muestra el error en la conexion e indica cual ha sido.
+        catch(PDOException $e){
+            die($e->getMessage());
+        }
+     }
+ }
+ //Se manda a llamar a la conexion.
+ conection::get_conection();
 ?>
